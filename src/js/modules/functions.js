35bfeda90bab098bcad2288703f7c27dc180433b
Vladislav Ometov <ometov96@mail.ru>
2022-12-24 05:42:13 +0700

    Фиксы адаптива, готов весь js

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,4 +1,6 @@
 /* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
+import events from "events";
+
 export function isWebp() {
 	// Проверка поддержки webp
 	function testWebP(callback) {
@@ -13,4 +15,55 @@ export function isWebp() {
 		let className = support === true ? 'webp' : 'no-webp';
 		document.documentElement.classList.add(className);
 	});
+}
+
+/* Мобильное меню */
+export function mobileMenu() {
+	const burgerButton = document.querySelector('.mobile_menu');
+	const menu = document.querySelector('.menu_list');
+
+	if (burgerButton) {
+		burgerButton.addEventListener("click", activateMobileMenu);
+
+		window.addEventListener('click', e => { // при клике в любом месте окна браузера
+			const target = e.target // находим элемент, на котором был клик
+			if (!target.closest('.mobile_menu') && !target.closest('.menu_list')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
+				document.body.classList.remove('open-menu');
+				menu.classList.remove('open-menu');
+			}
+		})
+	}
+
+	function activateMobileMenu () {
+		if (document.body.classList.contains('open-menu')){
+			document.body.classList.remove('open-menu');
+			menu.classList.remove('open-menu');
+		} else {
+			document.body.classList.add('open-menu');
+			menu.classList.add('open-menu');
+		}
+	}
+}
+
+/* Форма далее и подтвердить */
+export function formSteps() {
+	const formBlock = document.querySelector('.form_screen .form form');
+
+	const nextStep = formBlock.querySelector('.next_step');
+	const prevStep = formBlock.querySelector('.prev_step');
+
+	if (formBlock) {
+		nextStep.addEventListener("click", nextStepClick);
+		prevStep.addEventListener("click", prevStepClick);
+	}
+
+	function nextStepClick () {
+		formBlock.querySelector('.form--first').classList.remove('active');
+		formBlock.querySelector('.form--second').classList.add('active');
+	}
+
+	function prevStepClick () {
+		formBlock.querySelector('.form--second').classList.remove('active');
+		formBlock.querySelector('.form--first').classList.add('active');
+	}
 }
